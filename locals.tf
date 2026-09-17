85c926f36c25e718d94588cf2ef2397e78dd2111
demo@microsoft.com <demo@microsoft.com>
2026-09-17 06:56:25 -0700

    Add locals.tf [skip ci]

---
+++ locals.tf
@@ -0,0 +1,65 @@
+locals {
+  const = {
+    connectivity = {
+      virtual_wan        = "virtual_wan"
+      hub_and_spoke_vnet = "hub_and_spoke_vnet"
+      none               = "none"
+    }
+  }
+}
+
+locals {
+  connectivity_enabled                    = var.connectivity_type != local.const.connectivity.none
+  connectivity_virtual_wan_enabled        = var.connectivity_type == local.const.connectivity.virtual_wan
+  connectivity_hub_and_spoke_vnet_enabled = var.connectivity_type == local.const.connectivity.hub_and_spoke_vnet
+}
+
+# Build an implicit dependency on the resource groups
+locals {
+  resource_groups = {
+    resource_groups = module.resource_groups
+  }
+  hub_and_spoke_networks_settings = merge(module.config.outputs.hub_and_spoke_networks_settings, local.resource_groups)
+  hub_virtual_networks            = (merge({ vnets = module.config.outputs.hub_virtual_networks }, local.resource_groups)).vnets
+  virtual_wan_settings            = merge(module.config.outputs.virtual_wan_settings, local.resource_groups)
+  virtual_hubs                    = (merge({ vhubs = module.config.outputs.virtual_hubs }, local.resource_groups)).vhubs
+  route_maps = merge([
+    for virtual_hub_key, virtual_hub in local.virtual_hubs : {
+      for route_map_key, route_map in try(virtual_hub.route_maps, {}) :
+      "${virtual_hub_key}-${route_map_key}" => merge(route_map, { virtual_hub_key = virtual_hub_key })
+    }
+  ]...)
+}
+
+locals {
+  policy_default_values = { for k, v in try(module.config.outputs.management_group_settings.policy_default_values, {}) : k => jsonencode({ value = v }) }
+  policy_assignments_to_modify = { for management_group_key, management_group_value in try(module.config.outputs.management_group_settings.policy_assignments_to_modify, {}) : management_group_key => {
+    policy_assignments = { for policy_assignment_key, policy_assignment_value in try(management_group_value.policy_assignments, {}) : policy_assignment_key => {
+      enforcement_mode        = try(policy_assignment_value.enforcement_mode, null)
+      identity                = try(policy_assignment_value.identity, null)
+      identity_ids            = try(policy_assignment_value.identity_ids, null)
+      parameters              = try({ for parameter_key, parameter_value in try(policy_assignment_value.parameters, {}) : parameter_key => jsonencode({ value = parameter_value }) }, null)
+      not_scopes              = try(policy_assignment_value.not_scopes, null)
+      non_compliance_messages = try(policy_assignment_value.non_compliance_messages, null)
+      resource_selectors      = try(policy_assignment_value.resource_selectors, null)
+      overrides               = try(policy_assignment_value.overrides, null)
+      creation_enabled        = try(policy_assignment_value.creation_enabled, true)
+    } }
+  } }
+}
+
+locals {
+  management_group_dependencies = [
+    var.management_resources_enabled ? module.management_resources : null,
+    local.connectivity_hub_and_spoke_vnet_enabled ? module.hub_and_spoke_vnet : null,
+    local.connectivity_virtual_wan_enabled ? module.virtual_wan : null
+  ]
+}
+
+locals {
+  # Remove keys if subscription_id is not a valid GUID, i.e. null or empty string
+  subscription_placement_filtered = {
+    for k, v in module.config.outputs.management_group_settings.subscription_placement : k => v
+    if can(regex("^[a-f\\d]{4}(?:[a-f\\d]{4}-){4}[a-f\\d]{12}$", v.subscription_id))
+  }
+}
